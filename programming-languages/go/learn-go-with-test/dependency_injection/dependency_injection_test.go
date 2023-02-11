7770ed49483a12fd1ee11660e976c43b12566859
rdanielwetan <daniel.saputra@ralali.com>
2023-02-11 18:09:02 +0700

    go dependency injection test

---
+++ programming-languages/go/learn-go-with-test/dependency_injection/dependency_injection_test.go
@@ -0,0 +1,18 @@
+package dependency_injection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Daniel")
+
+	got := buffer.String()
+	want := "Hello, Daniel"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
