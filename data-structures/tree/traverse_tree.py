a9a2b62382397809f49f94adcfd1c7cb80f512a2
Daniel Saputra <danielwetan.io@gmail.com>
2024-03-28 22:20:47 +0700

    feat: traverse tree

---
+++ data-structures/tree/traverse_tree.py
@@ -0,0 +1,50 @@
+class TreeNode:
+  def __init__(self, value):
+    self.value = value 
+    self.left = None
+    self.right = None
+
+def inorder_traversal(root):
+  if root:
+    inorder_traversal(root.left)
+    print(root.value, end=" ")
+    inorder_traversal(root.right)
+
+def preorder_traversal(root):
+    if root:
+        print(root.value, end=" ")
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        print(root.value, end=" ")
+
+# inorder_traversal performs an inorder traversal (left-root-right) of the tree.
+# preorder_traversal performs a preorder traversal (root-left-right) of the tree.
+# postorder_traversal performs a postorder traversal (left-right-root) of the tree.
+
+# Example tree
+#        1
+#      /   \
+#     2     3
+#    / \   / \
+#   4   5 6   7
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+print("Inorder Traversal:")
+inorder_traversal(root)
+print("\nPreorder Traversal:")
+preorder_traversal(root)
+print("\nPostorder Traversal:")
+postorder_traversal(root)
+
