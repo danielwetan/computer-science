2e73c3ff3079b03fa0ac3454997991d528e46785
Daniel Saputra <danielwetan.io@gmail.com>
2024-09-30 00:27:31 +0700

    feat: rename point of sale to shortener

--- projects/point-of-sale/webapp/app/dashboard/layout.tsx
+++ projects/point-of-sale/webapp/app/dashboard/layout.tsx
@@ -42,7 +42,7 @@ export default async function DashboardLayout({
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">Acme Inc</span>
+              <span className="">Shortener</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
@@ -98,7 +98,7 @@ export default async function DashboardLayout({
                   className="flex items-center gap-2 text-lg font-semibold"
                 >
                   <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Acme Inc</span>
+                  <span className="sr-only">Shortener</span>
                 </Link>
                 <Link
                   href="#"
