758248e9955b302d74e5957744c1d75268c8d43a
Daniel Saputra <danielwetan.io@gmail.com>
2024-10-02 21:26:54 +0700

    feat: move project to software engineering repo

--- projects/shortener/backend/model/config.go
+++
@@ -1,5 +0,0 @@
-package model
-
-const (
-	DBTypeMySQL = "mysql"
-)
--- projects/shortener/webapp/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/auth/login-form";
-
-export default function LoginPage() {
-  return (
-    <div className="flex w-full min-h-screen items-center justify-center p-6">
-      <LoginForm />
-    </div>
-  );
-}
